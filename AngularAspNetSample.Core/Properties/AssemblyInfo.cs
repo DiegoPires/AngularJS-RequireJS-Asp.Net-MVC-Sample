7145455c91ca0cd821c672b6e13de5396d752bbb
DiegoPires <diego_silva_pires@outlook.com>
2014-04-09 11:50:25 -0300

    csproj file included

--- AngularAspNetSample.Core/Properties/AssemblyInfo.cs
+++ AngularAspNetSample.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77e4ef33-951d-4788-866f-cfc30885fff2")]
+[assembly: Guid("55a5d70c-de54-46c5-a2ac-9f305d073251")]
 
 // Version information for an assembly consists of the following four values:
 //
